2688f6abb811a5bcb175bd1f10f9a5ac79d3dd07
Joe Nelson <jnelson@onshift.com>
2018-08-31 14:28:51 -0400

    solution using inheritance

--- gilded_rose_test.py
+++ gilded_rose_test.py
@@ -1,7 +1,7 @@
-from item import Item
-from gilded_rose import GildedRose
+from unittest import TestCase
 
-from unittest import TestCase, skip
+from gilded_rose import GildedRose
+from item import ConjuredItem, ImprovingItem, LegendaryItem, RegularItem
 
 gilded_rose = GildedRose()
 
@@ -12,7 +12,7 @@ class GildedRoseTest(TestCase):
         self.items = []
 
     def test_regular_items_decrease_by_one(self):
-        self.items.append(Item("+5 Dexterity Vest", 10, 20))
+        self.items.append(RegularItem("+5 Dexterity Vest", 10, 20))
         gilded_rose.update_quality(self.items)
         expected = {'sell_in': 9, 'quality': 19}
         item = self.items[0]
@@ -20,8 +20,8 @@ class GildedRoseTest(TestCase):
         self.assertEqual(item.sell_in, expected['sell_in'])
 
     def test_quality_goes_up_for_improving_products(self):
-        self.items.append(Item("Aged Brie", 20, 30))
-        self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 20, 30))
+        self.items.append(ImprovingItem("Aged Brie", 20, 30))
+        self.items.append(ImprovingItem("Backstage passes to a TAFKAL80ETC concert", 20, 30))
         gilded_rose.update_quality(self.items)
         expected = [
               {'sell_in': 19, 'quality': 31},
@@ -34,8 +34,8 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
     def test_quality_goes_up_by_two_for_improving_products_with_10_days_or_less_left(self):
-        self.items.append(Item("Aged Brie", 10, 34))
-        self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 8, 30))
+        self.items.append(ImprovingItem("Aged Brie", 10, 34))
+        self.items.append(ImprovingItem("Backstage passes to a TAFKAL80ETC concert", 8, 30))
         gilded_rose.update_quality(self.items)
         expected = [
             {'sell_in': 9, 'quality': 36},
@@ -48,8 +48,8 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
     def test_quality_goes_up_by_three_for_improving_products_with_5_days_or_less_left(self):
-        self.items.append(Item("Aged Brie", 4, 11))
-        self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 5, 15))
+        self.items.append(ImprovingItem("Aged Brie", 4, 11))
+        self.items.append(ImprovingItem("Backstage passes to a TAFKAL80ETC concert", 5, 15))
         gilded_rose.update_quality(self.items)
         expected = [
             {'sell_in': 3, 'quality': 14},
@@ -62,8 +62,8 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
     def test_quality_and_sellin_decrease_twice_as_fast_after_sell_by(self):
-        self.items.append(Item("+5 Dexterity Vest", 0, 20))
-        self.items.append(Item("Conjured Mana Cake", 0, 6))
+        self.items.append(RegularItem("+5 Dexterity Vest", 0, 20))
+        self.items.append(RegularItem("Conjured Mana Cake", 0, 6))
         gilded_rose.update_quality(self.items)
         expected = [
             {'sell_in': -1, 'quality': 18},
@@ -76,8 +76,8 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
     def test_backstage_passes_and_brie_go_to_quality_zero_after_sell_by(self):
-        self.items.append(Item("Aged Brie", 0, 20))
-        self.items.append(Item("Backstage passes to a TAFKAL80ETC concert", 0, 20))
+        self.items.append(ImprovingItem("Aged Brie", 0, 20))
+        self.items.append(ImprovingItem("Backstage passes to a TAFKAL80ETC concert", 0, 20))
         gilded_rose.update_quality(self.items)
         expected = [
             {'sell_in': -1, 'quality': 0},
@@ -90,7 +90,7 @@ class GildedRoseTest(TestCase):
             self.assertEqual(item.sell_in, expectation['sell_in'])
 
     def test_sulfuras_the_immutable(self):
-        self.items.append(Item("Sulfuras, Hand of Ragnaros", 0, 80))
+        self.items.append(LegendaryItem("Sulfuras, Hand of Ragnaros", 0, 80))
         gilded_rose.update_quality(self.items)
         expected = {'sell_in': 0, 'quality': 80}
         item = self.items[0]
@@ -98,18 +98,17 @@ class GildedRoseTest(TestCase):
         self.assertEqual(item.sell_in, expected['sell_in'])
 
     def test_quality_does_not_increase_past_50(self):
-        self.items.append(Item("Aged Brie", 4, 49))
+        self.items.append(ImprovingItem("Aged Brie", 4, 49))
         gilded_rose.update_quality(self.items)
         expected = {'sell_in': 3, 'quality': 50}
         item = self.items[0]
         self.assertEqual(item.quality, expected['quality'])
         self.assertEqual(item.sell_in, expected['sell_in'])
 
-    @skip
     def test_conjured_items_decrease_in_quality_twice_as_fast(self):
-        self.items.append(Item("Conjured Mana Cake", 3, 6))
+        self.items.append(ConjuredItem("Conjured Mana Cake", 3, 6))
         gilded_rose.update_quality(self.items)
-        expected = {'sell_in': 2, 'quality': 2}
+        expected = {'sell_in': 2, 'quality': 4}
         item = self.items[0]
         self.assertEqual(item.quality, expected['quality'])
         self.assertEqual(item.sell_in, expected['sell_in'])
--- item.py
+++ item.py
@@ -4,3 +4,53 @@ class Item:
         self.sell_in = sell_in
         self.quality = quality
 
+
+class RegularItem(Item):
+    def __init__(self, *args):
+        super(RegularItem, self).__init__(*args)
+        self.QUALITY_RATE = -1
+        self.SELL_IN_RATE = -1
+
+    def age(self):
+        self._add_sell_in(self.SELL_IN_RATE)
+        self._add_quality(self._get_quality_rate())
+
+    def _add_sell_in(self, amount):
+        self.sell_in = self.sell_in + amount
+
+    def _add_quality(self, amount):
+        new_quality = self.quality + amount
+        self.quality = new_quality if new_quality < 50 else 50
+
+    def _get_quality_rate(self):
+        return self.QUALITY_RATE if self.sell_in > 0 else self.QUALITY_RATE * 2
+
+
+class ImprovingItem(RegularItem):
+    def __init__(self, *args):
+        super(ImprovingItem, self).__init__(*args)
+        self.QUALITY_RATE = 1
+
+    def _get_quality_rate(self):
+        if self.sell_in > 10:
+            return self.QUALITY_RATE
+        if self.sell_in > 5:
+            return self.QUALITY_RATE * 2
+        if self.sell_in > 0:
+            return self.QUALITY_RATE * 3
+        return self.quality * -1
+
+
+class LegendaryItem(RegularItem):
+    def __init__(self, *args):
+        super(LegendaryItem, self).__init__(*args)
+        self.SELL_IN_RATE = 0
+
+    def _add_quality(self, amount):
+        pass
+
+
+class ConjuredItem(RegularItem):
+    def __init__(self, *args):
+        super(ConjuredItem, self).__init__(*args)
+        self.QUALITY_RATE = self.QUALITY_RATE * 2
